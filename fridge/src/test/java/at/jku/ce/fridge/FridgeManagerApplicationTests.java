0c20ac1d6edd201e5cd05d64e8ca205a06ebb180
laurenzhinterholzer22 <68244290+laurenzhinterholzer22@users.noreply.github.com>
2022-04-16 12:14:40 +0200

    first commit of Rest Service

---
+++ fridge/src/test/java/at/jku/ce/fridge/FridgeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package at.jku.ce.fridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
